d086760096792a50606dd4757415648adacacea0
SlavaGolovatskyu <slavik.golovatskyu@gmail.com>
2021-12-12 21:59:21 +0200

    deleted not used files and created starting files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+  const [currentStep, setCurrentStep] = React.useState("");
 
-export default App;
+  const onStart = () => {
+    setCurrentStep("game");
+  };
+
+  return <div className={styles.App}></div>;
+};
