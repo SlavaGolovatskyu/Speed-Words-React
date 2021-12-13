7e4ef213bc71d7ea1789a7b0e53ee569e43513c4
SlavaGolovatskyu <slavik.golovatskyu@gmail.com>
2021-12-13 15:42:16 +0200

    made Game components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from "react";
-import { Hello } from "./components/Hello";
-import styles from "./App.module.scss";
+import React from 'react';
+import { Hello } from './components/Hello';
+import { Game } from './components/Game';
+import styles from './App.module.scss';
 
 export default function App() {
-  const [currentStep, setCurrentStep] = React.useState("hello");
+  const [currentStep, setCurrentStep] = React.useState('hello');
+  const [currentIndex, setCurrentIndex] = React.useState(0);
 
   const onStart = () => {
-    setCurrentStep("game");
+    setCurrentStep('game');
+  };
+
+  const onFinish = () => {
+    setCurrentStep('result');
+  };
+
+  const incrementCurrentIndex = () => {
+    setCurrentIndex((prev) => prev + 1);
   };
 
   return (
     <div>
-      {currentStep === "hello" && <Hello onStart={onStart} />}
-      {currentStep === "game" && <h1>Это игра</h1>}
-      {currentStep === "result" && <h1>Это результат</h1>}
+      {currentStep === 'hello' && <Hello onStart={onStart} />}
+      {currentStep === 'game' && (
+        <Game
+          incrementIndex={incrementCurrentIndex}
+          currentIndex={currentIndex}
+          onFinish={onFinish}
+        />
+      )}
+      {currentStep === 'result' && <h1>Это результат</h1>}
     </div>
   );
 }
---
+++ src/components/Game.jsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+const arr = [
+  'Наверное вы ещё готовы такой музыке вашим детям она понравится',
+  'Тото у меня такое ощущение что мы больше Канзасе',
+  'Не судите меня за прошлое я живу там больше.',
+  'Как принято считать стремящиеся вытеснить традиционное производство нанотехнологии обнародованы',
+  'Не следует однако забывать том что выбранный нами инновационный',
+  'Сегодняшний день она планировала завершить порцией тако',
+  'У Грей намечалось свадьбы пенсии ей было далеко',
+  'На третьем этаже новая компания айтишники',
+  'Двери лифта открылись втором этаже',
+  'Она протирала стекла своих черепаховых очков сводила глаз Дженнифер',
+  'В отличие от мужчин женщины оставляли свою личность вместе ключами документами и неоплаченными счетами за электричество',
+  'у Грей так имелось достаточно информации чтобы начать работать',
+  'Люди вокруг шмыгали носами них слезились глаза Грей поежилась',
+  'Вот начались вопросы ведь Грей глаза слезятся легкие чистые',
+  'Так отец Грей Виктор говорит хорошеньких девушках вроде Изабель Линкольн',
+  'Пройдя через двойные двери мимо людей истекавших кровью мимо астматиков гудящих аппаратов Грей наконец оказалась кабинете Иана',
+  'Капля пота стекала виску смахивала',
+];
+
+const generateNewIndex = (len) => {
+  return Math.floor(Math.random() * len);
+};
+
+const getRandomWord = () => {
+  const firstIndex = generateNewIndex(arr.length);
+  const wordArr = arr[firstIndex].split(' ');
+  const secondIndex = generateNewIndex(wordArr.length);
+  return wordArr[secondIndex];
+};
+
+export const Game = ({ incrementIndex, currentIndex, onFinish }) => {
+  const [inputValue, setInputValue] = React.useState('');
+  const [timeLeft, setTimeLeft] = React.useState(30);
+  const [currentWord, setCurrentWord] = React.useState('');
+  const [isError, setIsError] = React.useState(false);
+
+  if (!currentWord) {
+    setCurrentWord(getRandomWord());
+  }
+
+  const onChangeInput = (event) => {
+    const { value } = event.target;
+
+    if (value === currentWord) {
+      incrementIndex();
+      setInputValue('');
+      setCurrentWord(getRandomWord());
+      return;
+    }
+
+    if (currentWord.startsWith(value)) {
+      if (isError) {
+        setIsError(false);
+      }
+    } else {
+      setIsError(true);
+    }
+
+    if (!value.trim()) {
+      setIsError(false);
+    }
+
+    setInputValue(value);
+  };
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => {
+        const newTime = prev - 1;
+        if (newTime <= 0) {
+          onFinish();
+        }
+        return newTime;
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={styles.Game}>
+      <p className={styles.GameTitle}>Введите слово</p>
+      <h2 className={styles.GameWord}>{currentWord}</h2>
+      <input
+        value={inputValue}
+        onChange={onChangeInput}
+        placeholder="Введите слово"
+        autoFocus
+        className={styles.GameInput + ' ' + (isError ? styles.error : null)}
+      />
+      <div className={styles.MainInformation}>
+        <div className={styles.GameRow}>
+          <p>Осталось времени:</p>
+          <h3 className={styles.timeLeft}>{timeLeft} сек.</h3>
+        </div>
+        <div className={styles.GameRow}>
+          <p>Введено слов:</p>
+          <h3 className={styles.words}>{currentIndex}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
